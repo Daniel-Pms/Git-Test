d706188f4ffdc4e4475d9fe8924eb1a21bcd0ee5
Daniel-Pms <38088657+Daniel-Pms@users.noreply.github.com>
2018-10-17 19:53:03 -0500

    issue fixed

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -30,8 +30,14 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  static int i;
+  if(i<182){
+  ++i;
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+  }
 }
+
+
